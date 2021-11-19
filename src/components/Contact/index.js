0f4d48a82114d0d740f383a5aa246d37692ed1a6
Ryan Dungan <rd3@knights.ucf.edu>
2021-11-19 12:18:14 -0500

    contact section is done, just needes functionality

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -6,15 +6,14 @@ function Contact() {
 
       <div className="contact-left">
           <h2>Get in touch with <span className="span-color">me!</span></h2>
-          <p>Have any questions? Send me a message!</p>
+          <p>Feel free to connect on social media.</p>
           <div>
               <a href="https://www.linkedin.com/in/ryan-dungan-5352a9211/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in fa-7x"></i></a>
           <a href="https://github.com/imSpires" target="_blank" rel="noopener noreferrer"><i class="fab fa-github fa-7x"></i></a>
           </div>
       </div>
         <div className="contact-right">
-        <div className="contact-box">
-            <div className="contact-box-right">
+            <div className="contact-box">
                 <h2>Send me a message!</h2>
                 <p>I will get back to you as soon as possible!</p>
                 <input type="text" className="field" placeholder="Your Name" />
@@ -22,8 +21,6 @@ function Contact() {
                 <textarea className="field" placeholder="Message" />
                 <button className="contactbtn btn">Send!</button>
             </div>
-
-        </div>
         </div>
 
     </section>
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -3,12 +3,13 @@ import React from 'react';
 function Footer() {
     return (
       <footer class="footer">
-          <div class="icons">
-          <a href="https://github.com/imSpires" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
-          <a href="https://www.linkedin.com/in/ryan-dungan-5352a9211/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
-          </div>
           <div>
-          &copy; 2021 <strong>SPIRES</strong>, Inc.
+          &copy; 2021 Ryan Dungan. All rights reserved.
+          </div>
+
+          <div class="icons">
+            <a href="https://github.com/imSpires" target="_blank" rel="noopener noreferrer"><i class="fab fa-github"></i></a>
+            <a href="https://www.linkedin.com/in/ryan-dungan-5352a9211/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin-in"></i></a>
           </div>
       </footer>
     );
